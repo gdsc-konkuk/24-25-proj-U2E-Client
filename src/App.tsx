ef763e7d5b9b3754c0ea47dfd5dca322177595f4
xoxun <00xxs2@naver.com>
2025-03-07 23:31:18 +0900

    [feat] test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Welcome! This is the U2E web site.</h1>;
+  return (
+    <h1>
+      Welcome! This is the U2E web site.<span>hi ㅎㅎ</span>
+    </h1>
+  );
 }
 
 export default App;
