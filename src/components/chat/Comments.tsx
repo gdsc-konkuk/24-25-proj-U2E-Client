3e26405cdda2467165d8e971f834161941461ce8
Rune <sungjong5k@naver.com>
2025-05-06 16:33:09 +0900

    feat: add test request in Comment.tsx

--- src/components/chat/Comments.tsx
+++ src/components/chat/Comments.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import useApi from "../../hooks/useApi";
+
 function Comments() {
+  const { fetchTest } = useApi();
+
+  useEffect(() => {
+    fetchTest();
+  }, []);
   return <div>Comments</div>;
 }
 
