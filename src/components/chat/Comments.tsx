347ee616fc2ae270749073342f27330eaa30b317
Rune <sungjong5k@naver.com>
2025-05-06 21:51:12 +0900

    feat: add UserInput component

--- src/components/chat/Comments.tsx
+++ src/components/chat/Comments.tsx
@@ -1,37 +1,20 @@
 import { useParams } from "react-router-dom";
-import {
-  useCommentsQuery,
-  useCreateCommentMutation,
-} from "../../hooks/useCommentsQuery";
+import { useCommentsQuery } from "../../hooks/useCommentsQuery";
 import CommentCard from "./CommentCard";
 import styled from "styled-components";
 
 function Comments() {
-  const newsId = useParams().newsId || 1;
+  const newsId = Number(useParams().newsId) || 1;
   const { data, isLoading, error } = useCommentsQuery(newsId);
-  const { mutate: createComment, error: createError } =
-    useCreateCommentMutation();
 
   if (isLoading) return <div>댓글을 불러오는 중...</div>;
   if (error) return <div>댓글을 불러오는 데 문제가 발생했습니다.</div>;
-
-  const handleCreate = () => {
-    createComment({
-      userId: 123,
-      userName: "천재",
-      newsId: 1,
-      contents: "새로운 댓글 내용입니다!",
-    });
-  };
 
   return (
     <Container>
       {data?.data.commentList.map((comment) => (
         <CommentCard key={comment.commentId} {...comment} />
       ))}
-
-      <button onClick={handleCreate}>{"댓글 작성"}</button>
-      {createError && <p>댓글 작성 중 오류가 발생했습니다.</p>}
     </Container>
   );
 }
--- src/components/chat/UserInput.tsx
+++ src/components/chat/UserInput.tsx
@@ -1,5 +1,74 @@
-function UserInput() {
-  return <div>UserInput</div>;
-}
+import styled from "styled-components";
+import { rowFlex } from "../../styles/flexStyles";
+import SendIcon from "../../assets/svgs/SendIcon.svg?react";
+import { useState } from "react";
+import { useCreateCommentMutation } from "../../hooks/useCommentsQuery";
+import theme from "../../styles/theme";
+import { useParams } from "react-router-dom";
+
+const UserInput = () => {
+  const newsId = Number(useParams().newsId) || 1;
+  const [userInput, setUserInput] = useState<string>("");
+
+  const { mutate: createComment, error: createError } =
+    useCreateCommentMutation();
+
+  const sendComment = () => {
+    createComment({
+      userId: 123,
+      userName: "천재",
+      newsId,
+      contents: userInput,
+    });
+
+    setUserInput("");
+  };
+
+  if (createError) {
+    alert("댓글 작성에 실패했습니다.");
+  }
+
+  return (
+    <Container>
+      <StyledInput
+        type="text"
+        placeholder="댓글을 입력하세요."
+        value={userInput}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setUserInput(e.target.value)
+        }
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          if (e.key === "Enter") {
+            sendComment();
+          }
+        }}
+      />
+      <SendIconWrapper onClick={sendComment}>
+        <SendIcon width={"24px"} height={"24px"} />
+      </SendIconWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  box-sizing: border-box;
+  padding: 0 10px;
+  width: 90%;
+  height: 45px;
+  border-radius: 30px;
+  background: ${theme.colors.white};
+  ${rowFlex({ justify: "center", align: "center" })};
+`;
+
+const StyledInput = styled.input`
+  width: calc(100% - 30px);
+  padding: 0 10px;
+  outline: none;
+  border: none;
+`;
+
+const SendIconWrapper = styled.div`
+  ${rowFlex({ justify: "center", align: "center" })};
+`;
 
 export default UserInput;
