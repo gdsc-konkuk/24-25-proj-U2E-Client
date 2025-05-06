17850fe50981a48b8c1e16234b230556d1b0a136
Rune <sungjong5k@naver.com>
2025-05-06 17:59:48 +0900

    chore: refine comments handler with detailed scenarios

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,58 @@
-// src/mocks/handlers.js
 import { http, HttpResponse } from "msw";
 
-const API_URL = "https://test";
+const baseURL =
+  import.meta.env.VITE_APP_NODE === "development"
+    ? "https://test"
+    : "https://us2earth.click/";
 
 export const handlers = [
-  http.get(`${API_URL}/comments/`, () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      name: "이성종",
-    });
+  http.get(`${baseURL}/comments/:newsId`, ({ params }) => {
+    const { newsId } = params;
+
+    if (newsId && parseInt(newsId.toString()) > 0) {
+      return HttpResponse.json({
+        code: 200,
+        message: "OK",
+        data: {
+          commentList: [
+            {
+              userId: 1,
+              commentId: 1,
+              contents: "안녕하세요, 저는 댓글입니다.",
+            },
+            {
+              userId: 2,
+              commentId: 2,
+              contents: "기후위기가 정말 심각하네요.",
+            },
+            {
+              userId: 3,
+              commentId: 3,
+              contents: "우리 모두 아름다운 세상을 만들어나가요.",
+            },
+            {
+              userId: 4,
+              commentId: 4,
+              contents: "기후위기 해결을 위해 모두가 함께 노력해야 합니다.",
+            },
+            {
+              userId: 4,
+              commentId: 5,
+              contents: "일단 나부터.",
+            },
+          ],
+        },
+      });
+    }
+
+    return HttpResponse.json(
+      {
+        code: 3000,
+        message:
+          "비밀번호는 영어와 숫자를 포함해서 8자 이상 16자 이내로 입력해주세요.",
+        timestamp: Date.now(),
+      },
+      { status: 400 }
+    );
   }),
 ];
