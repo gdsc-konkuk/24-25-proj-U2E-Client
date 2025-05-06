93065624df36fbe57fba0eca83f9e178a744c7a9
Rune <sungjong5k@naver.com>
2025-05-06 20:29:25 +0900

    feat: add createComment function

--- src/api/commentApi.ts
+++ src/api/commentApi.ts
@@ -1,4 +1,5 @@
-import { CommentResponse } from "../types/response";
+import { CommentResponse, CreateCommentResponse } from "../types/response";
+import { CreateCommentRequest } from "../types/request";
 import apiClient from "./client";
 
 /**
@@ -8,5 +9,26 @@ import apiClient from "./client";
  */
 export const fetchComments = async (newsId: string | number) => {
   const response = await apiClient.get<CommentResponse>(`/comments/${newsId}`);
+  return response.data;
+};
+
+/**
+ * 댓글 작성 API
+ * @param params 댓글 작성에 필요한 파라미터 (userId, newsId, contents)
+ * @returns 작성된 댓글 정보
+ */
+export const createComment = async ({
+  userId,
+  newsId,
+  contents,
+}: CreateCommentRequest) => {
+  const response = await apiClient.post<CreateCommentResponse>(
+    `/comments/${newsId}`,
+    {
+      userId,
+      newsId,
+      contents,
+    }
+  );
   return response.data;
 };
