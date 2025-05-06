f73470c78c9909afaf0303bf2695950ccc76a27f
Rune <sungjong5k@naver.com>
2025-05-06 17:56:13 +0900

    feat: wrap App component with React Query QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import theme from "./styles/theme";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
 import { GlobalStyle } from "./styles/globalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
