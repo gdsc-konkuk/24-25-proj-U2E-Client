28fa2d15e1cddba48c1f550fceccbeecbeddd556
Rune <sungjong5k@naver.com>
2025-05-08 18:28:37 +0900

    feat: change fetchComment's base api url

--- src/api/commentApi.ts
+++ src/api/commentApi.ts
@@ -1,6 +1,7 @@
 import { CommentResponse, CreateCommentResponse } from "../types/response";
 import { CreateCommentRequest } from "../types/request";
 import apiClient from "./client";
+import axios from "axios";
 
 /**
  * 특정 뉴스의 댓글 목록을 조회하는 API
@@ -8,7 +9,9 @@ import apiClient from "./client";
  * @returns 댓글 목록
  */
 export const fetchComments = async (newsId: number) => {
-  const response = await apiClient.get<CommentResponse>(`/comments/${newsId}`);
+  const response = await axios.get<CommentResponse>(
+    `https://us2earth.click/comments/${newsId}`
+  );
   return response.data;
 };
 
