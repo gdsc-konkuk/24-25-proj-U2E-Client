7f25d6fd8167dacdbdca1cf604061c61bb5fd4ff
Rune <sungjong5k@naver.com>
2025-05-08 18:08:59 +0900

    feat: add useNewsContentsQuery

--- src/hooks/useNewsQuery.ts
+++ src/hooks/useNewsQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchRecentNews } from "../api/newsApi";
+import { fetchNewsContents, fetchRecentNews } from "../api/newsApi";
 
 /**
  * 최근 뉴스 목록을 조회하는 커스텀 훅
@@ -9,5 +9,18 @@ export const useRecentNewsQuery = () => {
   return useQuery({
     queryKey: ["recentNews"],
     queryFn: fetchRecentNews,
+  });
+};
+
+/**
+ * 뉴스 ID를 통해 뉴스 본문을 조회하는 커스텀 훅
+ * @param newsId 뉴스 ID
+ * @returns 뉴스 상세 정보 쿼리 결과
+ */
+export const useNewsContentsQuery = (newsId: number) => {
+  return useQuery({
+    queryKey: ["newsContents", newsId],
+    queryFn: () => fetchNewsContents(newsId),
+    enabled: !!newsId,
   });
 };
