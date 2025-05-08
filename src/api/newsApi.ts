3d452a5e90106f2c384215b0f2e4a88570b4dea7
Rune <sungjong5k@naver.com>
2025-05-08 18:08:48 +0900

    feat: add fetchNewsContents function

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -1,13 +1,26 @@
 import axios from "axios";
-import { NewsResponse } from "../types/response";
+import { RecentNewsResponse, NewsResponse } from "../types/response";
 
 /**
- * 최근 뉴스 목록을 조회하는 API
+ * @description 최근 뉴스 목록을 조회하는 API
  * @returns 최근 뉴스 목록
  */
 export const fetchRecentNews = async () => {
-  const response = await axios.get<NewsResponse>(
+  const response = await axios.get<RecentNewsResponse>(
     `https://us2earth.click/news/lately`
+  );
+  return response.data;
+};
+
+/**
+ *
+ * @param newsId 뉴스 ID
+ * @description 뉴스 ID를 통해 뉴스 본문을 조회하는 API
+ * @returns 뉴스 상세 정보
+ */
+export const fetchNewsContents = async (newsId: number) => {
+  const response = await axios.get<NewsResponse>(
+    `https://us2earth.click/news/${newsId}`
   );
   return response.data;
 };
